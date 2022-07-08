ef59c3fd5b769d22e852cd474724f40360a71215
壷井翔大 <tsuboishoudai@tsuboishoudainoMacBook-Air.local>
2022-07-08 21:22:11 +0900

    課題提出_2日目

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-for (let i = 1; i < 51; i++) {
-  count = i;
+let i;
+
+for(i = 1; i <= 50; i++){
+    if(i % 10 == 0 ){
+        console.log("今" + i + "回ループしました。");
+    }
+
+    if(i % 4 == 0){
+        console.log("4で割れる数です。" + i);
+    }
 }
 
-if (count == 10) {
-  console.log("今" + count + "回ループしました。");
-} else if (count == 20) {
-  console.log("今" + count + "回ループしました。");
-} else if (count == 30) {
-  console.log("今" + count + "回ループしました。");
-} else if (count == 40) {
-  console.log("今" + count + "回ループしました。");
-} else if (count == 50) {
-  console.log("今" + count + "回ループしました。");
-}
-
-if (count % 4 == 0) {
-  console.log("4で割れる数です。" + count + "");
-}
-
-if (count == 50) {
-  alert("50回カウントが終わりました。");
-}
+alert(i + "回カウントが終わりました。")
